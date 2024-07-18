233c24f0d26768eb12b9233c9c2630286bca2ddf
FacundoPino <facunahuelp@gmail.com>
2024-07-18 19:13:38 -0300

    Mejoras

---
+++ script.js
@@ -0,0 +1,13 @@
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+    
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
